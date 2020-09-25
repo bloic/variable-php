09df25a74ad7fb6ca4817618682c55e543b7addd
bloic <loic.benos@gmail.com>
2020-09-25 16:28:56 +0200

    ajout du fichier index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    $nameMovie = 'Indiana Jones and the Last Crusade' ;
+    $watch = false;
+    
+    $year = 1989;
+    $note = 8.2;
+    
+    echo "Movie: ",$nameMovie,"<br>";
+    if ($watch == false){ 
+    echo "seen:","not seen", "<br>"; }
+    echo "Year: ", $year, "<br>";
+    echo "Note: ", $note, "br"; 
+    ?>
+</body>
+</html>
